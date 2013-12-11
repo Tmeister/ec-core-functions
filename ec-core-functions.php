2740d8b63c50bdf0a2b67a8d669577c030d2cce6
Enrique Chavez <noone@tmeister.net>
2013-12-11 13:28:15 -0600

    Add theme options
    Signed-off-by: Enrique Chavez <noone@tmeister.net>

---
+++ ec-core-functions.php
@@ -0,0 +1,57 @@
+<?php
+/*
+Plugin Name: Theme Core Functions
+Description: This plugin contains all the core function for Enrique's themes in order to data will not be lost when the user changes the theme.
+Plugin URI: http://enriquechavez.co
+Author: Enrique Chavez
+Author URI: http://enriquechavez.co
+Version: 1.0
+License: GPL2
+Text Domain: ec-core-functions
+Domain Path: /lang
+*/
+/*
+
+    Copyright (C) 2013  Enrique Chavez  noone@tmeister.net
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License, version 2, as
+    published by the Free Software Foundation.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+/**
+ * Theme Core Functions
+ */
+class ChavezCoreFunctions
+{
+	var $theme_options;
+	function __construct()
+	{
+		global $theme_options;
+		add_action('after_setup_theme', array(
+			$this,
+			'load_theme_options'
+		));
+	}
+	function load_theme_options()
+	{
+		/**
+		 * Adding the Redux Framework and Theme Configuration Panel
+		 */
+		if (!class_exists('ReduxFramework') && file_exists(dirname(__FILE__) . '/vendor/ReduxFramework/ReduxCore/framework.php')) {
+			require_once (dirname(__FILE__) . '/vendor/ReduxFramework/ReduxCore/framework.php');
+		}
+		if (!isset($cendres) && file_exists(dirname(__FILE__) . '/lib/theme-options-config.php')) {
+			require_once (dirname(__FILE__) . '/lib/theme-options-config.php');
+		}
+	}
+}
+new ChavezCoreFunctions;
